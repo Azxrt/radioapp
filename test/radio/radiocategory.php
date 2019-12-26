bf6cda46fb5370ac74dedfa5b011dccba225edca
Azxrt <46218793+Azxrt@users.noreply.github.com>
2019-12-26 15:40:03 +0300

    Add files via upload

---
+++ test/radio/radiocategory.php
@@ -0,0 +1,155 @@
+{
+  "radio_category": [
+    {
+      "cid": "1",
+      "category_name": "Muzyka Polska",
+      "category_image": "polska.png"
+    },
+    {
+      "cid": "2",
+      "category_name": "Muzyka Filmowa",
+      "category_image": "Soundtracks.jpg"
+    },
+    {
+      "cid": "3",
+      "category_name": "Radia Polonijne",
+      "category_image": "polonia.png"
+    },
+    {
+      "cid": "4",
+      "category_name": "RAP - HIP HOP",
+      "category_image": "Rap.jpg"
+    },
+    {
+      "cid": "5",
+      "category_name": "Reggae",
+      "category_image": "reg.jpg"
+    },
+    {
+      "cid": "6",
+      "category_name": "Disco Polo",
+      "category_image": "discopolo.jpg"
+    },
+    {
+      "cid": "7",
+      "category_name": "Love Songs",
+      "category_image": "love.jpg"
+    },
+    {
+      "cid": "8",
+      "category_name": "Dla Dzieci",
+      "category_image": "kids2.jpg"
+    },
+    {
+      "cid": "9",
+      "category_name": "Rock",
+      "category_image": "Rock.jpg"
+    },
+    {
+      "cid": "10",
+      "category_name": "Blues",
+      "category_image": "blues-music.jpg"
+    },
+    {
+      "cid": "12",
+      "category_name": "Chill",
+      "category_image": "chill.jpg"
+    },
+    {
+      "cid": "13",
+      "category_name": "Jazz",
+      "category_image": "jazz.jpeg"
+    },
+    {
+      "cid": "14",
+      "category_name": "Dance",
+      "category_image": "International.jpg"
+    },
+    {
+      "cid": "15",
+      "category_name": "POP",
+      "category_image": "Pop.jpg"
+    },
+    {
+      "cid": "16",
+      "category_name": "Electro",
+      "category_image": "techno.jpg"
+    },
+    {
+      "cid": "17",
+      "category_name": "News - Talk",
+      "category_image": "Talk.jpg"
+    },
+    {
+      "cid": "18",
+      "category_name": "Klasyczna",
+      "category_image": "Classical.jpg"
+    },
+    {
+      "cid": "19",
+      "category_name": "HITY",
+      "category_image": "hity.jpeg"
+    },
+    {
+      "cid": "20",
+      "category_name": "Oldies",
+      "category_image": "old.jpg"
+    },
+    {
+      "cid": "22",
+      "category_name": "Kultura - Muzyka",
+      "category_image": "culture.jpg"
+    },
+    {
+      "cid": "23",
+      "category_name": "Sport",
+      "category_image": "sportradio.png"
+    },
+    {
+      "cid": "24",
+      "category_name": "Muzyka - Informacje",
+      "category_image": "musicinfo.jpeg"
+    },
+    {
+      "cid": "25",
+      "category_name": "Greatest Hits",
+      "category_image": "greathits.jpg"
+    },
+    {
+      "cid": "26",
+      "category_name": "Hard",
+      "category_image": "Metal.jpg"
+    },
+    {
+      "cid": "27",
+      "category_name": "Soul Music",
+      "category_image": "ambient1.jpg"
+    },
+    {
+      "cid": "28",
+      "category_name": "Katolickie",
+      "category_image": "katolickie.jpg"
+    },
+    {
+      "cid": "29",
+      "category_name": "Club - Party - Trance",
+      "category_image": "club.jpg"
+    },
+    {
+      "cid": "30",
+      "category_name": "Inne",
+      "category_image": "pobrane.jpeg"
+    },
+    {
+      "cid": "31",
+      "category_name": "Open FM",
+      "category_image": "openfm.jpg"
+    },
+    {
+      "cid": "32",
+      "category_name": "ŚWIĘTA",
+      "category_image": "wesolych.jpg"
+    }
+  ],
+  "success": 1
+}
---
+++ test/radio/radiostation.php
@@ -0,0 +1,305 @@
+{
+  "list": [
+    {
+      "id": "5035",
+      "category_id": "32",
+      "radio_name": "Świąteczny Chillout by Chillizet",
+      "genre_id": "18",
+      "radio_image": "chill1.png",
+      "radio_url": "https://ch03.cdn.eurozet.pl/CHISWI.mp3",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5034",
+      "category_id": "32",
+      "radio_name": "Na Gwiazdkę Hity",
+      "genre_id": "18",
+      "radio_image": "hity.png",
+      "radio_url": "https://stream.open.fm/90",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5033",
+      "category_id": "32",
+      "radio_name": "Na Gwiazdkę",
+      "genre_id": "18",
+      "radio_image": "nagwiazdke.png",
+      "radio_url": "https://stream.open.fm/87",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5032",
+      "category_id": "32",
+      "radio_name": "Świąteczne Klasyki ZET",
+      "genre_id": "18",
+      "radio_image": "klasyki.png",
+      "radio_url": "https://zg04.cdn.eurozet.pl/ZGLSWI.mp3",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5031",
+      "category_id": "32",
+      "radio_name": "Na Gwiazdkę Kolędy",
+      "genre_id": "18",
+      "radio_image": "koledy.png",
+      "radio_url": "https://stream.open.fm/89",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5030",
+      "category_id": "32",
+      "radio_name": "Na Gwiazdkę Po Polsku",
+      "genre_id": "18",
+      "radio_image": "popolsku1.png",
+      "radio_url": "https://stream.open.fm/56",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5029",
+      "category_id": "32",
+      "radio_name": "Na Gwiazdkę Jazz Classic",
+      "genre_id": "18",
+      "radio_image": "jazz.png",
+      "radio_url": "https://stream.open.fm/88",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5028",
+      "category_id": "3",
+      "radio_name": "Vienna Plus",
+      "genre_id": "18",
+      "radio_image": "viennaplus.jpg",
+      "radio_url": "http://radio.viennaplus.at:8000/;",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5027",
+      "category_id": "32",
+      "radio_name": "RMF Święta",
+      "genre_id": "6",
+      "radio_image": "rmfswita.png",
+      "radio_url": "http://217.74.72.10/SWIETA",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5026",
+      "category_id": "32",
+      "radio_name": "ZET Święta",
+      "genre_id": "7",
+      "radio_image": "zetswieta.png",
+      "radio_url": "http://zet02.cdn.eurozet.pl:8462/ZETSWI.mp3",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5025",
+      "category_id": "32",
+      "radio_name": "RMF Kolędy",
+      "genre_id": "6",
+      "radio_image": "rmfkoledy.jpg",
+      "radio_url": "http://195.150.20.8/KOLEDY",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5024",
+      "category_id": "15",
+      "radio_name": "COSMO",
+      "genre_id": "18",
+      "radio_image": "cosmo.png",
+      "radio_url": "http://addrad.io/4WRN5w",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5023",
+      "category_id": "15",
+      "radio_name": "Radio ZET Futbol",
+      "genre_id": "7",
+      "radio_image": "fut.png",
+      "radio_url": "https://al03.cdn.eurozet.pl/ZETFUT.mp3?ts=1526999136780",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5021",
+      "category_id": "15",
+      "radio_name": "Radio Mazury",
+      "genre_id": "14",
+      "radio_image": "maz.png",
+      "radio_url": "http://s1.slotex.pl:7186/stream/1/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5020",
+      "category_id": "15",
+      "radio_name": "Radio Hair Trendy",
+      "genre_id": "7",
+      "radio_image": "hair.png",
+      "radio_url": "http://91.232.4.33:8049/stream",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5019",
+      "category_id": "15",
+      "radio_name": "Radio Opole 2",
+      "genre_id": "8",
+      "radio_image": "opole2.png",
+      "radio_url": "http://dab.radio.opole.pl:8045/1",
+      "view": "0",
+      "frequency": "fm"
+    },
+    {
+      "id": "5018",
+      "category_id": "9",
+      "radio_name": "UJOT FM",
+      "genre_id": "6",
+      "radio_image": "ujot.png",
+      "radio_url": "http://radio.wzks.uj.edu.pl:8000/ujotfm128",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5017",
+      "category_id": "29",
+      "radio_name": "Toksyna FM - Psytrance",
+      "genre_id": "18",
+      "radio_image": "toks.png",
+      "radio_url": "http://s01.toksyna.fm:8030/stream/1/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5016",
+      "category_id": "6",
+      "radio_name": "Radio FJV FM",
+      "genre_id": "18",
+      "radio_image": "fjm.png",
+      "radio_url": "http://80.211.255.32:8000/stream/1/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5015",
+      "category_id": "12",
+      "radio_name": "Nirvana Meditation",
+      "genre_id": "18",
+      "radio_image": "nirw.png",
+      "radio_url": "http://radio.108.pl:8004/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5014",
+      "category_id": "28",
+      "radio_name": "Katolickie Radio Diecezji Plockiej",
+      "genre_id": "7",
+      "radio_image": "krdp.png",
+      "radio_url": "http://s1.slotex.pl:7762/;",
+      "view": "0",
+      "frequency": "fm"
+    },
+    {
+      "id": "5012",
+      "category_id": "12",
+      "radio_name": "Arche Radio",
+      "genre_id": "16",
+      "radio_image": "arche.png",
+      "radio_url": "http://s1.slotex.pl:7068/stream/1/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5011",
+      "category_id": "15",
+      "radio_name": "Stacjaimpreza",
+      "genre_id": "12",
+      "radio_image": "imp.png",
+      "radio_url": "http://185.24.219.206:9090/stream/1/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5010",
+      "category_id": "1",
+      "radio_name": "OpenFM - Polskie Ballady",
+      "genre_id": "7",
+      "radio_image": "pl.png",
+      "radio_url": "http://stream.open.fm/165",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5009",
+      "category_id": "31",
+      "radio_name": "OpenFM - Do Auta Club",
+      "genre_id": "7",
+      "radio_image": "auto.png",
+      "radio_url": "http://stream.open.fm/114",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5008",
+      "category_id": "19",
+      "radio_name": "Hits by Radio ZET",
+      "genre_id": "7",
+      "radio_image": "hits.png",
+      "radio_url": "http://zethit-01.cdn.eurozet.pl:8428/;?",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5007",
+      "category_id": "14",
+      "radio_name": "Max Radio Polska",
+      "genre_id": "18",
+      "radio_image": "max1.png",
+      "radio_url": "http://s1.slotex.pl:7216/stream/1/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5006",
+      "category_id": "29",
+      "radio_name": "Hopstacja",
+      "genre_id": "1",
+      "radio_image": "hop.png",
+      "radio_url": "http://4stream.pl:18226/stream/1/",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5005",
+      "category_id": "17",
+      "radio_name": "Halo Radio",
+      "genre_id": "7",
+      "radio_image": "halo.jpg",
+      "radio_url": "https://s3.radio.co/s6b9e36d1c/listen",
+      "view": "0",
+      "frequency": "online"
+    },
+    {
+      "id": "5004",
+      "category_id": "9",
+      "radio_name": "Antyradio Warszawa",
+      "genre_id": "7",
+      "radio_image": "anty.png",
+      "radio_url": "https://an02.cdn.eurozet.pl/ant-waw.mp3",
+      "view": "0",
+      "frequency": "fm"
+    }
+  ],
+  "success": 1
+}
